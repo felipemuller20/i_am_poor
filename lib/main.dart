317d501dfb5856c93137408075ca22fd3716893d
felipe <felipeavmuller@gmail.com>
2023-10-21 15:34:00 -0300

    i am poor cents

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Center(child: const Text('I Am Poor')),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: const Center(
+          child: Image(
+        image: AssetImage('images/1cent.png'),
+      )),
+    ),
+  ));
+}
